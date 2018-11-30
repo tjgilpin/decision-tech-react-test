bb2a2d333891d2f9335f9846bd89eb6d7264aef0
Tim Gilpin <1889642+tjgilpin@users.noreply.github.com>
2018-11-30 16:31:32 +0000

    feat: set up state, svg and toggle for NavMenu component

--- src/index.js
+++ src/index.js
@@ -110,7 +110,6 @@ class DealTable extends React.Component {
 
     return (
       <div className="deal-grid-content">
-        <DealFilter/>
         <table className="deal-grid-table">
           <thead>
             <tr>
@@ -133,11 +132,29 @@ class DealTable extends React.Component {
 }
 
 class NavMenu extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      visible: false
+    }
+  }
+  toggleVisible () {
+    this.setState({
+      visible: !this.state.visible
+    })
+  }
+
   render() {
     return (
-      <ul>
-        <li></li>
-      </ul>
+      <nav className="deal-grid-nav" onClick={this.toggleVisible.bind(this)}>
+        <svg height="32px" id="Layer_1" version="1.1" viewBox="0 0 32 32" width="32px" xmlns="http://www.w3.org/2000/svg">
+          <path d="M4,10h24c1.104,0,2-0.896,2-2s-0.896-2-2-2H4C2.896,6,2,6.896,2,8S2.896,10,4,10z M28,14H4c-1.104,0-2,0.896-2,2  s0.896,2,2,2h24c1.104,0,2-0.896,2-2S29.104,14,28,14z M28,22H4c-1.104,0-2,0.896-2,2s0.896,2,2,2h24c1.104,0,2-0.896,2-2  S29.104,22,28,22z" />
+        </svg>
+        {
+          this.state.visible &&
+            <DealFilter/>
+        }
+      </nav>
     );
   }  
 }
